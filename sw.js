26b08afef85d0aa8677d4c8d3988e2011466364d
leonardobravot-ctrl <leonardobravot@yahoo.com>
2026-09-22 13:23:51 -0600

    Refresh cache for V5.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='air3s-v5-1';
+const C='air3s-v5-2';
 const CORE=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(CORE))).then(()=>self.skipWaiting()));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
